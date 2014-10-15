a52da31624bf6f17d0da5028fce83fdb1f735f30
Houndsto0th <bry.kinnear@gmail.com>
2014-10-15 15:10:53 -0600

    broken quotes method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,14 @@
 class PagesController < ApplicationController
 
+
+  def index
+    @walt = Quote.new("Walt Whitman", ["Simplicity is the glory of expression.", "Be curious, not judgemental."])
+    @durden = Quote.new("Tyler Durden", ["It's only after we've lost everything that we're free to do anything.", "The things you own end up owning you"])
+    @warhol = Quote.new("Andy Warhol", ["They always say time changes things, but you actually have to change them yourself.", "Art is what you can get away with."])
+    @carroll = Quote.new("Louis Carroll", ["But I don't want to go among mad people.", "Why, sometimes I've believed as many as six impossible things before breakfast"])
+
+    @authors_and_quotes = [@walt, @durden, @warhol, @carroll]
+
+  end
+
 end
---
+++ app/models/quotes.rb
@@ -0,0 +1,11 @@
+class Quote
+  attr_accessor :author, :quote
+
+  def initialize(author, quote)
+    self.author = author
+    self.quote = quote
+  end
+
+
+
+end
