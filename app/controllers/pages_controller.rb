2300b0b7a066936370f29b2c44c97cac41fb4b20
Houndsto0th <bry.kinnear@gmail.com>
2014-10-20 12:58:39 -0600

    added koans to faq page until more direction is given

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,11 +12,15 @@ class PagesController < ApplicationController
 
 
   def faq
-    @question1 = Faq.new("test", "test")
-    @question2 = Faq.new("test", "test")
-    @question3 = Faq.new("test", "test")
+    @question1 = Faq.new("What is the way?", "What a fine mountain this is.")
+    @question2 = Faq.new("Why don't you go to bed", "The hall is very dark.")
+    @question3 = Faq.new("A man sits on top of a hundred foot pole, how can he go farther up?", "He should reach for enlightment, then he can stand up in all four corners of the sky at once.")
+    @question4 = Faq.new("What is the way?", "It is right before your eyes")
+    @question5 = Faq.new("Why do I not see it for myself?", "Because you are thinking of yourself")
+    @question6 = Faq.new("What about you: do you see it?", "So long as you see double, saying I don't and you do, and so on, your eyes are clouded")
 
-    @frequently_asked_questions = [@question1, @question2, @question3]
+
+    @frequently_asked_questions = [@question1, @question2, @question3, @question4, @question5, @question6]
   end
 
 
