ea7006a64203979097988f9d4680fd4f0ffb8c8b
Houndsto0th <bry.kinnear@gmail.com>
2014-10-22 13:35:29 -0600

    added sorting by completeness

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.order(:due_date).all
+    @tasks = Task.order(:complete, :due_date).all
 
   end
 
