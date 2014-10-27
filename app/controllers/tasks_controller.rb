5b39ade04527dad056d7950ceb9b3ec1292626bc
Houndsto0th <bry.kinnear@gmail.com>
2014-10-27 15:11:00 -0600

    added users database and links on footer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
   # GET /tasks/1
   # GET /tasks/1.json
   def show
+    
   end
 
   # GET /tasks/new
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "User Saved: Success!"
+    end
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+
+  private
+    def set_user
+      @task = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
+end
