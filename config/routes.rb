b5135f66631dfbf91c643dda8faa7d409e2900f3
Houndsto0th <bry.kinnear@gmail.com>
2014-11-03 14:50:16 -0700

    created user authentication, users can now sign in and sign out as well as create themselves

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   resources :tasks do
     patch :toggle, on: :member
   end
+  
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
+
+
   resources :projects
   root "pages#index"
   get "about" => "pages#about", as: :about
---
+++ db/migrate/20141103201005_add_user_pword.rb
@@ -0,0 +1,5 @@
+class AddUserPword < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
