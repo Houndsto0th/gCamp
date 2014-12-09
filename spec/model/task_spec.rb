66dd62e8de03270a69d59c62e18304f38ef5505d
Houndsto0th <bry.kinnear@gmail.com>
2014-12-08 19:42:51 -0700

    fixed date.today issue in task model spec

--- spec/model/task_spec.rb
+++ spec/model/task_spec.rb
@@ -1,33 +1,43 @@
 require 'rails_helper'
 
-
 describe Task do
+  before do
+    Task.delete_all
+    Project.delete_all
+  end
   it "validates the date of a task" do
+    project = create_project
     task = Task.new(
-      due_date: Date.today-7
+      description: Faker::Lorem.sentence,
+      due_date: "12/12/2012",
+      project_id: project.id,
     )
+
+
     task.valid?
     expect(task.errors[:due_date].present?).to eq(true)
-    task.due_date = Date.today
+    task.due_date = "12/12/3012"
     task.valid?
     expect(task.errors[:due_date].present?).to eq(false)
   end
 
   include ActiveSupport::Testing::TimeHelpers
   it "confirms existing tasks can have a date in the past." do
+    project = create_project
     task = Task.new
-    travel_to 3.years.ago do
-      task.description = "Oh man, Time Travel"
-      task.due_date = Date.today
+    travel_to Time.new("1/1/2010") do
+      task.description= "Oh man, Time Travel"
+      task.due_date= Date.today
       task.save
     end
 
     task.valid?
+    p task.errors
     expect(task.errors[:due_date].present?).to eq(false)
-    task.due_date = 1/1/2000
+    task.due_date = "1/1/2000"
     task.valid?
     expect(task.errors[:due_date].present?).to eq(false)
-    task.due_date = 1/1/3000
+    task.due_date = "1/1/3000"
     task.valid?
     expect(task.errors[:due_date].present?).to eq(false)
   end
